8714747d78fe0f34d1b85e2ffeb25e0c1918d7a2
Jonghyun-K1m <jonghyun424@naver.com>
2020-02-12 17:06:54 +0900

    c

--- m.js
+++ m.js
@@ -1,20 +1,25 @@
+
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var port=3000;
+
 var server=http.createServer(function(req,res){
 
     var postdata='';
     req.on('data',function(data){
         postdata=postdata+data;
-    })
+       // console.log(postdata);
+   })
     req.on('end',function(){
 
         var parsedQuery=qs.parse(postdata);
         console.log(parsedQuery);
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.end('ss');
+        //res.end('ss');
+        //res.write('ss');
+     
     });
 });
 
